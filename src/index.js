1dbc580a76b5f6f4027d9d7002df5ca4df59c175
vera <darthyoon@gmail.com>
2021-03-29 21:39:06 +0300

    Block, slider, pictures

--- src/index.js
+++ src/index.js
@@ -47,3 +47,38 @@ $('.space-slider').slick({
   ]
 })
 
+
+
+$('.gallery-slider').slick({
+  /* centerMode: true,
+  centerPadding: '0px', */
+  slidesToShow: 3,
+  arrows: true,
+  appendArrows:'.gallery-section__arrows',
+  prevArrow:'<span class="slider-arrow gallery-section__arrow_prev"></span>',
+  nextArrow:'<span class="slider-arrow gallery-section__arrow_next"></span>',
+  responsive: [
+    {
+      breakpoint: 1080,
+      settings: {
+        slidesToShow: 2,
+        centerMode: false,
+      }
+    },{
+      breakpoint: 730,
+      settings: {
+        slidesToShow: 1,
+        centerMode: false,
+      }
+    },
+  ]
+})
+
+
+$('.gallery-slider .slick-active:first').addClass('slick-select')
+
+$('.gallery-slider').on('afterChange', function () {
+  $('.gallery-slider .slick-active').removeClass('slick-select')
+  $('.gallery-slider .slick-active:first').addClass('slick-select')
+  console.log( $('.gallery-slider .slick-active:first'))
+})
